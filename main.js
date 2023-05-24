a52c732f266c2536d888d5910952529d2da7d67e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-05-24 14:16:30 -0600

    Add popUp Mobile Menu for the home page

--- main.js
+++ main.js
@@ -77,6 +77,31 @@ const featuredSpeakers = [
     const speakerTemplate = document.getElementById('speaker-template');
     const speakersContainer = document.querySelector('.speakers-ul');
     appendSpeakers(featuredSpeakers, speakersContainer);
+    //mobile drop menu;
+    function displayMobileMenu() {
+        console.log('runing displayMobileMenu')
+        const topNavBar = document.querySelector('#nav-bar-1');
+        const mainNavBar = document.querySelector('#nav-bar-2');
+        const exitContainer = document.querySelector('#li-exit')
+        exitContainer.classList.remove('hidden')
+        topNavBar.classList.add('show-mobile-menu-top');
+        mainNavBar.classList.add('show-mobile-menu-main');
+    }
 
-    /* -------- Ends Featured Speakers Section -------- */
+    function closeMobileMenu() {
+        console.log('runing close mobile menu')
+        const topNavBar = document.querySelector('#nav-bar-1');
+        const mainNavBar = document.querySelector('#nav-bar-2');
+        const exitContainer = document.querySelector('#li-exit')
+        topNavBar.classList.remove('show-mobile-menu-top');
+        mainNavBar.classList.remove('show-mobile-menu-main');
+        exitContainer.classList.add('hidden')
+    }
+
+
+    const hamburgerButton = document.querySelector('#hamburger-button');
+    const closeMobileButton = document.querySelector('#exitMobile');
+    hamburgerButton.addEventListener('click', displayMobileMenu )
+    closeMobileButton.addEventListener('click', closeMobileMenu)
+
 
