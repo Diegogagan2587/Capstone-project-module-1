6a3093a286f08f2730d7d3b1a2ac3ebbb80aed34
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-05-24 18:59:59 -0600

    fix Lintern Errors

--- js/navigation.js
+++ js/navigation.js
@@ -1,32 +1,27 @@
 function displayMobileMenu() {
-    console.log('runing displayMobileMenu')
-    const topNavBar = document.querySelector('#nav-bar-1');
-    const mainNavBar = document.querySelector('#nav-bar-2');
-    const exitContainer = document.querySelector('#li-exit')
-    exitContainer.classList.remove('hidden')
-    topNavBar.classList.add('show-mobile-menu-top');
-    mainNavBar.classList.add('show-mobile-menu-main');
+  const topNavBar = document.querySelector('#nav-bar-1');
+  const mainNavBar = document.querySelector('#nav-bar-2');
+  const exitContainer = document.querySelector('#li-exit');
+  exitContainer.classList.remove('hidden');
+  topNavBar.classList.add('show-mobile-menu-top');
+  mainNavBar.classList.add('show-mobile-menu-main');
 }
 
 function closeMobileMenu() {
-    console.log('runing close mobile menu')
-    const topNavBar = document.querySelector('#nav-bar-1');
-    const mainNavBar = document.querySelector('#nav-bar-2');
-    const exitContainer = document.querySelector('#li-exit')
-    topNavBar.classList.remove('show-mobile-menu-top');
-    mainNavBar.classList.remove('show-mobile-menu-main');
-    exitContainer.classList.add('hidden')
+  const topNavBar = document.querySelector('#nav-bar-1');
+  const mainNavBar = document.querySelector('#nav-bar-2');
+  const exitContainer = document.querySelector('#li-exit');
+  topNavBar.classList.remove('show-mobile-menu-top');
+  mainNavBar.classList.remove('show-mobile-menu-main');
+  exitContainer.classList.add('hidden');
 }
-
 
 const hamburgerButton = document.querySelector('#hamburger-button');
 const closeMobileButton = document.querySelector('#exitMobile');
 const navListAnchors = document.getElementById('nav-bar').querySelectorAll('li');
-hamburgerButton.addEventListener('click', displayMobileMenu )
-closeMobileButton.addEventListener('click', closeMobileMenu)
-
-console.log(navListAnchors)
+hamburgerButton.addEventListener('click', displayMobileMenu);
+closeMobileButton.addEventListener('click', closeMobileMenu);
 
 for (let i = 0; i < navListAnchors.length; i += 1) {
-    navListAnchors[i].addEventListener('click', closeMobileMenu);
-  }
+  navListAnchors[i].addEventListener('click', closeMobileMenu);
+}
--- main.js
+++ main.js
@@ -1,83 +1,77 @@
-
 /* -------- Start Featured Speakers Section -------- */
-function speakerData (
-    img,
-    name,
-    ocupation,
-    moreInfo
+function SpeakerData(
+  img,
+  name,
+  ocupation,
+  moreInfo,
 ) {
-    this.img = img;
-    this.name = name, 
-    this.ocupation = ocupation;
-    this.moreInfo = moreInfo;
+  this.img = img;
+  this.name = name;
+  this.ocupation = ocupation;
+  this.moreInfo = moreInfo;
 }
 
 function createNewSpeakerFrom(obj) {
-    console.log('runing Create NewSpeakerFrom')
-    const newSpeaker = speakerTemplate.cloneNode(true);
-    newSpeaker.classList.remove('hidden');
+  const speakerTemplate = document.getElementById('speaker-template');
+  const newSpeaker = speakerTemplate.cloneNode(true);
+  newSpeaker.classList.remove('hidden');
 
-    //now we modfy the content for the new speaker;
-    newSpeaker.querySelector('img').src = `${obj.img}`;
-    newSpeaker.querySelector('h3').innerText = `${obj.name}`
-    newSpeaker.querySelector('.ocupation').innerText = `${obj.ocupation}`
-    newSpeaker.querySelector('.more-info').innerText = `${obj.moreInfo}`
-    return newSpeaker;
+  // now we modfy the content for the new speaker;
+  newSpeaker.querySelector('img').src = `${obj.img}`;
+  newSpeaker.querySelector('h3').innerText = `${obj.name}`;
+  newSpeaker.querySelector('.ocupation').innerText = `${obj.ocupation}`;
+  newSpeaker.querySelector('.more-info').innerText = `${obj.moreInfo}`;
+  return newSpeaker;
 }
 
-function appendSpeakers(arrOfdata,container){
-    console.log('running appendSpeakers')
-    for(let i = 0; i < arrOfdata.length; i += 1 ) {
-        const tempSpeaker = createNewSpeakerFrom(arrOfdata[i]);
-        if(i > 1 ) { tempSpeaker.classList.add('hide-or-show')};
-        container.appendChild(tempSpeaker);
-    }
+function appendSpeakers(arrOfdata, container) {
+  for (let i = 0; i < arrOfdata.length; i += 1) {
+    const tempSpeaker = createNewSpeakerFrom(arrOfdata[i]);
+    if (i > 1) { tempSpeaker.classList.add('hide-or-show'); }
+    container.appendChild(tempSpeaker);
+  }
 }
 
 const featuredSpeakers = [
-    new speakerData (
-        './img/featured-speakers/speaker_01.jpg',
-        'Diego Vidal',
-        'Software Developer trained from Microverse',
-        'From delivery guy on to Sofware Developer, he will have more than 4 years of experience in 2027',
-    ),
-    new speakerData (
-        './img/featured-speakers/speaker_02.png',
-        'Bill Gates',
-        'One of the richest man of the world, he started as a developer',
-        'Bill Gates co-founded Microsoft, led the tech revolution, became a billionaire, and dedicated his wealth to philanthropy and global health.',
-    ),
-    new speakerData (
-        './img/featured-speakers/speaker_03.png',
-        'Linux Torvalds',
-        'Works full-time on the Linux kernel as part of the Linux Foundation.',
-        'Linux Torvalds created the Linux kernel, revolutionizing open-source software and becoming an influential figure in the tech industry.',
-    ),
-    new speakerData (
-        './img/featured-speakers/speaker_04.jpg',
-        'Freddy Vega',
-        'Founder of the largest tech school in Spanish: Platzi',
-        'Freddie Vega founded Platzi, an online education platform, empowering thousands to learn new skills and pursue career opportunities.',
-    ),
-    new speakerData (
-        './img/featured-speakers/speaker_05.jpg',
-        'Richard Stallman',
-        ' head of the GNU Project, and in 2021 returned to the FSF board of directors',
-        'Richard Stallman is a renowned computer programmer and software activist who founded the Free Software Movement and initiated GNU project.',
-    ),
-    new speakerData (
-        './img/featured-speakers/speaker_06.jpg',
-        'James Gosling',
-        'father of the Java programming language,',
-        'James Gosling is a computer scientist known as the father of the Java programming language, contributing significantly to software development.',
-    ),
-]
+  new SpeakerData(
+    './img/featured-speakers/speaker_01.jpg',
+    'Diego Vidal',
+    'Software Developer trained from Microverse',
+    'From delivery guy on to Sofware Developer, he will have more than 4 years of experience in 2027',
+  ),
+  new SpeakerData(
+    './img/featured-speakers/speaker_02.png',
+    'Bill Gates',
+    'One of the richest man of the world, he started as a developer',
+    'Bill Gates co-founded Microsoft, led the tech revolution, became a billionaire, and dedicated his wealth to philanthropy and global health.',
+  ),
+  new SpeakerData(
+    './img/featured-speakers/speaker_03.png',
+    'Linux Torvalds',
+    'Works full-time on the Linux kernel as part of the Linux Foundation.',
+    'Linux Torvalds created the Linux kernel, revolutionizing open-source software and becoming an influential figure in the tech industry.',
+  ),
+  new SpeakerData(
+    './img/featured-speakers/speaker_04.jpg',
+    'Freddy Vega',
+    'Founder of the largest tech school in Spanish: Platzi',
+    'Freddie Vega founded Platzi, an online education platform, empowering thousands to learn new skills and pursue career opportunities.',
+  ),
+  new SpeakerData(
+    './img/featured-speakers/speaker_05.jpg',
+    'Richard Stallman',
+    ' head of the GNU Project, and in 2021 returned to the FSF board of directors',
+    'Richard Stallman is a renowned computer programmer and software activist who founded the Free Software Movement and initiated GNU project.',
+  ),
+  new SpeakerData(
+    './img/featured-speakers/speaker_06.jpg',
+    'James Gosling',
+    'father of the Java programming language,',
+    'James Gosling is a computer scientist known as the father of the Java programming language, contributing significantly to software development.',
+  ),
+];
 
-    // get the template for each speaker
-    const speakerTemplate = document.getElementById('speaker-template');
-    const speakersContainer = document.querySelector('.speakers-ul');
-    appendSpeakers(featuredSpeakers, speakersContainer);
-    //mobile drop menu;
-
-
-
+// get the template for each speaker
+const speakersContainer = document.querySelector('.speakers-ul');
+appendSpeakers(featuredSpeakers, speakersContainer);
+// mobile drop menu;
